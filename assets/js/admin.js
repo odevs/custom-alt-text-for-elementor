3af332bb2cdd12bc6b383536383b0c1e41ae3331
odevs <115993095+odevs@users.noreply.github.com>
2025-08-22 16:38:48 +0600

    Add files via upload

---
+++ assets/js/admin.js
@@ -0,0 +1,6 @@
+// jQuery(document).ready(function($) {
+//     // Add helpful tooltips or enhanced UX
+//     $('.elementor-control-custom_alt_text').on('focus', function() {
+//         // Show helpful hints
+//     });
+// });
